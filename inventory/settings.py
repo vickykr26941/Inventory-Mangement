c8fd21ab3b8ce02d17fc0c5435b82d3ef2f54df3
vickykr125 <vickykr26941@gmail.com>
2021-10-13 00:27:15 +0530

    added default profile image

--- inventory/settings.py
+++ inventory/settings.py
@@ -151,3 +151,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True 
 EMAIL_HOST_USER = 'vickykr26941@gmail.com'
 EMAIL_HOST_PASSWORD = 'vickykr123@'
+
+
+# admin user name : Admin 
+# password        : 
--- user/models.py
+++ user/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     staff = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
     address = models.CharField(max_length=200,null=True)
     phone  = models.CharField(max_length=20,null=True)
-    image = models.ImageField(default="vicky.jpg",upload_to="profile_images")
+    image = models.ImageField(default="avtar.jpg",upload_to="profile_images")
 
     def __str__(self):
         return f'{self.staff.username} profile'
